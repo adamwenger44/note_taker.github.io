b82da07d5dd3618e8c382f7ffbddd3aebd1f4b0b
Adam Wenger <adamwenger44@gmail.com>
2020-01-11 14:55:38 -0800

    added git ignore

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -12,25 +12,26 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
-// Routes
-// =============================================================
-
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../../index.html"));
-  });
+});
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../../notes.html"));
-  });
+});
 
-  app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../../../db/db.json"));
-  });
+});
+
+// Create New Note - takes in JSON input
 
 
-  
-  app.listen(PORT, function() {
+
+
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
